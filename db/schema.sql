546d562fa7ee88578ce403875c06af6a6a940265
RadioJackie <jacobpeters628@gmail.com>
2020-06-30 22:04:08 -0700

    Finished the schema sql file and the seeds sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,11 @@
-DROP DATABASE IF EXISTS burgers_db;
 
 CREATE DATABASE burgers_db;
 
 USE burgers_db;
 
 CREATE TABLE burgers(
-id INT NOT NULL,
-burger_name VARCHAR (30) NOT NULL,
-last_name VARCHAR (30) NOT NULL,
-role_id INT,
-manager_id INT,
-PRIMARY KEY (id)
-);
+id int NOT NULL AUTO_INCREMENT,
+	burger_name VARCHAR (255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+); 
